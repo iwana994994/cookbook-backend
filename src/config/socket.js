3bc29f643035b4295d04b657359635ba154cdb6e
iwana994994 <iwana994994@gmail.com>
2025-09-01 12:11:20 +0200

    d

--- src/config/socket.js
+++ src/config/socket.js
@@ -15,7 +15,7 @@ export const initializeSocket = (server) => {
     const userSocketMap = new Map();
     const userActivityMap = new Map();
 
-
+// check if token is valid SOCKET.IO IS REQUIRING TOKEN ALWAYS!!!
     io.use((socket, next) => {
   const token = socket.handshake.auth.token;
   if (isValidToken(token)) {
@@ -25,6 +25,9 @@ export const initializeSocket = (server) => {
     next(new Error("Unauthorized   😒😒😒"));
   }
 }); 
+
+
+
     // user are connected
     io.on("connection", (socket) => {
        socket.on("user_connected", (userId) => {
@@ -56,10 +59,10 @@ export const initializeSocket = (server) => {
         }    
         });
         socket.on("connect_error", (err) => {
-  console.error("Socket connect_error:    ", err);
+  console.error("Socket connect_error:  ⚔  ", err);
 });
 socket.on("error", (err) => {
-  console.error("Socket error:", err);
+  console.error("Socket error:  ⚔⚔⚔ ", err);
 });
 
 
